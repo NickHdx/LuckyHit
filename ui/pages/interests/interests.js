9fe061372d68d886d3f6983f32a51301c6f7802c
Theo Zhang <theo.zhang@morningstar.com>
2018-09-18 16:21:20 +0800

    Interests page.

---
+++ ui/pages/interests/interests.js
@@ -0,0 +1,74 @@
+// pages/interests.js
+Page({
+  data: {
+    textFields: [
+      { name: 'mala', value: '麻辣'},
+      { name: 'xianxiang', value: '咸鲜'},
+      { name: 'suanla', value: '酸辣'},
+      { name: 'suanxiang', value: '蒜香'},
+      { name: 'tiansuan', value: '甜酸'},
+      { name: 'congxiang', value: '葱香'},
+      { name: 'curry', value: '咖喱'},
+      { name: 'xiantian', value: '咸甜'},
+      { name: 'yuxiang', value: '鱼香'},
+      { name: 'others', value: '其他'}
+    ],
+    hidden: false
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
